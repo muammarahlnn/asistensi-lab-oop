6b7c6584b52037d72b92dc7249cca7edd1571edb
khaliza6604 <khalizatuljannah@gmail.com>
2023-03-27 15:03:14 +0800

    Kumpul tugas 3

---
+++ H071221101/Praktikum2/pekan 3.java
@@ -0,0 +1,11 @@
+public class pekan {
+    public String name;
+    private int age;
+    char gender; //'m' cowo, 'f' cewe
+
+    public pekan (String name, int age, char gender) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+    }
+}
